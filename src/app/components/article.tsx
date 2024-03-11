49916b08aa1ffc6612fbae6022e4380b88068b92
Steinar Bragi Sigurðarson <sbs88@hi.is>
2024-03-11 14:43:12 +0000

    tweak article and close button

--- src/app/components/article.tsx
+++ src/app/components/article.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import { Dispatch, SetStateAction } from "react";
+import { XCircleIcon } from "@heroicons/react/16/solid";
 
 /* eslint-disable @next/next/no-img-element */
 export default function Article({article, setSelected}: {
@@ -7,13 +9,14 @@ export default function Article({article, setSelected}: {
 }) {
     return <div >
         {article && <div className="fixed top-0 left-0 right-0 bottom-0 backdrop-blur-md bg-white/50 dark:bg-black/50">
-        <button onClick={() => setSelected(null)}>close</button>
+        <button className="h-10 w-10 m-5 right-0 absolute" onClick={() => setSelected(null)}><XCircleIcon /></button>
 
             <div className="flex flex-row justify-center">
                 <div className=" max-w-3xl ">
-                    <h1>{article.title}</h1>
-                    <img src={article.urlToImage} alt={article.title} />
-                    <p>{article.content}</p>
+                    <h1 className="text-3xl font-bold py-10">{article.title}</h1>
+                    <img className="rounded-3xl" src={article.urlToImage} alt={article.title} />
+                    <p className="py-5">{article.content}</p>
+                    <Link className="text-indigo-500 font-bold text-xl" href={article.url}>View Original</Link>
                 </div>
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const newsJson = await news.json()
 
   return (
-    <main className="min-h-screen p-24">
+    <main className="min-h-screen p-5 lg:p-24">
       <h2>News</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
       <ArticleGrid articles={newsJson.articles} />
