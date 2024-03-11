0fb3ab5fdc2256b4c18f5d35a77e1478f4db82e9
Steinar Bragi Sigurðarson <sbs88@hi.is>
2024-03-11 14:54:24 +0000

    tweaks

--- src/app/components/article.tsx
+++ src/app/components/article.tsx
@@ -10,9 +10,8 @@ export default function Article({article, setSelected}: {
     return <div >
         {article && <div className="fixed top-0 left-0 right-0 bottom-0 backdrop-blur-lg bg-white/60 dark:bg-black/60">
         <button className="h-10 w-10 m-5 right-0 absolute" onClick={() => setSelected(null)}><XCircleIcon /></button>
-
             <div className="flex flex-row justify-center">
-                <div className=" max-w-3xl ">
+                <div className=" p-5 max-w-3xl ">
                     <h1 className="text-3xl font-bold py-10">{article.title}</h1>
                     <img className="rounded-3xl" src={article.urlToImage} alt={article.title} />
                     <p className="py-5">{article.description}</p>
