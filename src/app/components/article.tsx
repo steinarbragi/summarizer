7945c02679249c7121c35da0be5696f76b5266a5
Steinar Bragi Sigurðarson <sbs88@hi.is>
2024-03-11 14:47:50 +0000

    tweak article backdrop blur

--- src/app/components/article.tsx
+++ src/app/components/article.tsx
@@ -8,14 +8,14 @@ export default function Article({article, setSelected}: {
     setSelected: Dispatch<SetStateAction<Article | null>> 
 }) {
     return <div >
-        {article && <div className="fixed top-0 left-0 right-0 bottom-0 backdrop-blur-md bg-white/50 dark:bg-black/50">
+        {article && <div className="fixed top-0 left-0 right-0 bottom-0 backdrop-blur-lg bg-white/60 dark:bg-black/60">
         <button className="h-10 w-10 m-5 right-0 absolute" onClick={() => setSelected(null)}><XCircleIcon /></button>
 
             <div className="flex flex-row justify-center">
                 <div className=" max-w-3xl ">
                     <h1 className="text-3xl font-bold py-10">{article.title}</h1>
                     <img className="rounded-3xl" src={article.urlToImage} alt={article.title} />
-                    <p className="py-5">{article.content}</p>
+                    <p className="py-5">{article.description}</p>
                     <Link className="text-indigo-500 font-bold text-xl" href={article.url}>View Original</Link>
                 </div>
             </div>
