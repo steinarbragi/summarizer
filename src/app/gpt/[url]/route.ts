2d7a6dd965bad06321d132c291d8f59944787cba
Steinar Bragi Sigurðarson <sbs88@hi.is>
2024-03-11 15:44:32 +0000

    fix api error

--- src/app/gpt/[url]/route.ts
+++ src/app/gpt/[url]/route.ts
@@ -1,7 +1,6 @@
-import { NextApiRequest } from 'next';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET (request:NextApiRequest, {params}: {params: {url: string}}){
+export async function GET (request:NextRequest, {params}: {params: {url: string}}){
     // TODO: send url to ChatGPT and ask for a summary
     return NextResponse.json({url: params.url})
 } 
