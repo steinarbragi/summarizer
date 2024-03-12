83538e550b96a33aafdd302fd331223ff47c4b5c
Steinar Bragi Sigurðarson <s@steinar.io>
2024-03-12 11:15:53 +0000

    tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,7 @@ export default async function Home() {
   return (
     <main className="min-h-screen p-5 lg:p-24">
       <h2>News</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        <ArticleGrid articles={newsJson.articles} />
-      </div>
+      <ArticleGrid articles={newsJson.articles} />
     </main>
   );
 }
