4d3c9050b7b5184055238abbf6cbc56d45b3e086
Steinar Bragi Sigurðarson <s@steinar.io>
2024-03-12 11:18:48 +0000

    fix grid again

--- src/app/components/articlegrid.tsx
+++ src/app/components/articlegrid.tsx
@@ -7,11 +7,11 @@ import Article from './article';
 export default function ArticleGrid({ articles }: { articles: Article[] }) {
   const [selected, setSelected] = useState<Article | null>(null);
   return (
-    <>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
       {articles.map((item: Article) => (
         <ArticleCard key={item.url} setSelected={setSelected} article={item} />
       ))}
       <Article article={selected} setSelected={setSelected} />
-    </>
+    </div>
   );
 }
