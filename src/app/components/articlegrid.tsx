9b163d1c76a61a0b2d29c4516d57d9255de98743
Steinar Bragi Sigurðarson <sbs88@hi.is>
2024-04-04 10:23:14 +0000

    tweak button styles, summary lengths

--- src/app/components/articlegrid.tsx
+++ src/app/components/articlegrid.tsx
@@ -9,23 +9,23 @@ export default function ArticleGrid({ articles }: { articles: Article[] }) {
   const [selectedCategory, setSelectedCategory] = useState<string>('funny');
   const [selectedLength, setSelectedLength] = useState<number>(50);
   const categoryList = ['funny', 'serious', 'pirate', 'aristocrat'];
-  const lengthList = [25, 50, 75];
+  const lengthList = [50, 100, 150];
   const lengthMap: { [key: number]: string } = {
-    25: 'Short',
-    50: 'Medium',
-    75: 'Long',
+    50: 'Short',
+    100: 'Medium',
+    150: 'Long',
   };
 
   return (
     <>
-      <div className="my-5">
+      <div>
         {categoryList.map(category => (
           <button
             key={category}
-            className={`m-2 ${
+            className={`m-2 rounded-full py-1 px-3 ${
               selectedCategory === category
-                ? 'bg-blue-500 text-white'
-                : 'bg-gray-200'
+                ? 'bg-indigo-500 text-white'
+                : 'bg-gray-500'
             }`}
             onClick={() => setSelectedCategory(category)}
           >
@@ -33,14 +33,14 @@ export default function ArticleGrid({ articles }: { articles: Article[] }) {
           </button>
         ))}
       </div>
-      <div className="my-5">
+      <div className="my-2">
         {lengthList.map(length => (
           <button
             key={length}
-            className={`m-2 ${
+            className={`m-2 rounded-full py-1 px-3 ${
               selectedLength === length
-                ? 'bg-blue-500 text-white'
-                : 'bg-gray-200'
+                ? 'bg-indigo-500 text-white'
+                : 'bg-gray-500'
             }`}
             onClick={() => setSelectedLength(length)}
           >
